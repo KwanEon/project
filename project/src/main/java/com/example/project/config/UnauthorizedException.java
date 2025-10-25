b73d670a607e66958d22e489160d6803bd6f23fe
KwanEon <buam1313@gmail.com>
2025-10-26 03:25:22 +0900

    스프링부트 불필요한 설정 삭제

--- project/src/main/java/com/example/project/config/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.config;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
