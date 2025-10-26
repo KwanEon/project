b7a91518d462a6499e5235f265e4ca8bc7ecff2e
KwanEon <buam1313@gmail.com>
2025-10-26 16:07:21 +0900

    주문 목록 조회 N+1 완화

--- project/src/main/java/com/example/project/repository/OrderRepository.java
+++ project/src/main/java/com/example/project/repository/OrderRepository.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Repository;
 import com.example.project.model.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.EntityGraph;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @EntityGraph(attributePaths = {"orderItems", "orderItems.product"})
     List<Order> findByUserId(Long userId);
 }
